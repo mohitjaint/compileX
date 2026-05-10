b7e3b049e883e1b8aac4a0c4fff3fc94061b2a31
mohitjaint <imohitjaint@gmail.com>
2026-05-10 12:57:53 +0530

    feat: add cookie-parser and cors middleware, set up app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,36 @@
-console.log('Starting backend server...');
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
+app.use(express.json({ 
+    limit: "16kb"
+})); //for forms
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+})); //for url params
+
+//limit is set to 16kb to prevent large payloads, which can help mitigate certain types of attacks and reduce server load.
+
+app.use(express.static("public")); //for static files ie serve files from backend/public
+
+app.use(cookieParser()); //for cookies
+
+
+//routes import 
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter);
+
+export default app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,20 @@
 console.log('Starting express backend server...');
+
+import connectDB from "./db/index.js";
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Failed to connect to the database:", err);
+        process.exit(1); // Exit with failure code
+    });
