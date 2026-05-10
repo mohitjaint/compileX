3c14b1050e4a97e1d06618adf0b9720fb07de04e
mohitjaint <imohitjaint@gmail.com>
2026-05-10 19:26:26 +0530

    feat: add generaRefreshToken and generateAccessToken methods in user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,10 +1,19 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const userSchema = new mongoose.Schema({
     username : {
         type: String,
         required: true,
         unique: true
+    },
+    fullName : {
+        type: String,
+        required: true
     },
     email : {
         type: String,
@@ -37,6 +46,38 @@ UserSchema.pre('save', async function(next) {
     this.passwordHash = bcrypt.hash(this.passwordHash, 10);
 });
 
+UserSchema.methods.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.passwordHash);
+}
+
+UserSchema.methods.generateAccessToken =  function() {
+    return  jwt.sign(
+        {
+            userId: this._id,
+            role: this.role,
+            fullName: this.fullName,
+            email: this.email
+        },
+        
+        process.env.ACCESS_TOKEN_SECRET,
+        { 
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN 
+        }
+    )
+}
+
+UserSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            userId: this._id,
+            email: this.email
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { 
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN 
+        }
+    );
+}
 
 
 const User = mongoose.model('User', userSchema);
