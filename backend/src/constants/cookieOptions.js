657ecdf97541707ae8f43484e77d744f03e97bf6
mohitjaint <imohitjaint@gmail.com>
2026-06-19 17:34:00 +0530

    fix: add missing newline at end of file in cookieOptions.js

--- backend/src/constants/cookieOptions.js
+++ backend/src/constants/cookieOptions.js
@@ -2,4 +2,5 @@ export const options = {
     httpOnly: true,
     secure: false, // Set to true in production with HTTPS
     sameSite: 'strict',
-};
+    maxAge: Number(process.env.REFRESH_COOKIE_MAX_AGE) 
+}
